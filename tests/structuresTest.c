dfd6b1aadde0c56ab612d7ebc7e43ebb31dc3a34
Collignon Nicolas <nicolas.collignon7@gmail.com>
2024-04-27 17:49:41 +0200

    m

---
+++ tests/structuresTest.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct
+{
+    int vie;
+    char    prenom[100];
+} Personnage;
+
+void    printPlayer(Personnage player)
+{
+    printf("------------\n");
+    printf("Prenom : %s\n", player.prenom);
+    printf("Vie : %d\n", player.vie);
+    printf("------------\n");
+
+}
+
+int main(void)
+{
+    Personnage player1;
+    player1.vie = 100;
+    strcpy(player1.prenom, "Prenom");
+
+    printPlayer(player1);
+    return (0);
+}
